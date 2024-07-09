ddb251f7f83420db8733736ba368b7bbfea68900
noman5945 <abdullahnoman.5945@gmail.com>
2024-07-09 09:00:19 +0600

    C++ compiler running on Laptop

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 /*
@@ -44,42 +44,53 @@ vector<vector<int>> threeSum(vector<int>& nums) {
 
 }
 */
-//Optimized Approach - O(n^2 logn + nlogn) - o(n^2 logn) time and O(n) space
-vector<vector<int>> threeSum(vector<int>& nums) {
-        int target = 0;
-        sort(nums.begin(), nums.end());
-        set<vector<int>> s;
-        vector<vector<int>> output;
-        for (int i = 0; i < nums.size(); i++){
-            int j = i + 1;
-            int k = nums.size() - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    s.insert({nums[i], nums[j], nums[k]});
-                    j++;
-                    k--;
-                } else if (sum < target) {
-                    j++;
-                } else {
-                    k--;
-                }
+// Optimized Approach - O(n^2 logn + nlogn) - o(n^2 logn) time and O(n) space
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    int target = 0;
+    sort(nums.begin(), nums.end());
+    set<vector<int>> s;
+    vector<vector<int>> output;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int j = i + 1;
+        int k = nums.size() - 1;
+        while (j < k)
+        {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum == target)
+            {
+                s.insert({nums[i], nums[j], nums[k]});
+                j++;
+                k--;
+            }
+            else if (sum < target)
+            {
+                j++;
+            }
+            else
+            {
+                k--;
             }
         }
-        for(auto triplets : s)
-            output.push_back(triplets);
-        return output;
+    }
+    for (auto triplets : s)
+        output.push_back(triplets);
+    return output;
 }
 
-int main(){
+int main()
+{
 
-    vector<int> nums={-1,0,1,2,-1,-4};
-    vector<vector<int>> ans=threeSum(nums);
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> ans = threeSum(nums);
 
-    for(int i=0;i<ans.size();i++){
-        for(int j=0;j<ans[i].size();j++){
-            cout<<ans[i][j]<<" ";
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
